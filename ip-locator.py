d5bebc7f419a3c58914285c3c95ba48e0e972437
aljavier <nulc0den4m3@gmail.com>
2014-09-03 14:24:26 -0400

    Change script file and improve the code

--- ip-locator.py
+++ ip-locator.py
@@ -1,88 +1,71 @@
 #!/usr/bin/env python
-#############################################
-#                                           #
-#         Simple GeoIP Script               #
-#	                by                        #
-#               n3x07                       #
-#   Visit http://netc0de.blogspot.com       #
-#                                           #
-#   Version 1.0  - Tested with: Python 2.7  #
-#                                           #
-#############################################
+# -*- encoding: utf-8 -*-
+#
+#  Coded by A. J. 
+#
+# This script use a geo webservice to locate an ip or host
+# provided by arguments in a commandline.
+#
+# Licensed under GPLv2.
+#
+# Python version: 2.7.x
+#
 
 import urllib2
 import sys
 import json
 import re
+import traceback
 
 
-class GeoIP:
-  def __init__(self, output="json"):
-    """ Constructor of the class GeoIP """
-    # freegeoip offer a free API for GeoIP
-    self.url = "http://freegeoip.net/%s/" % output
-
-  def validate_host(self, IP):
-    """ VAlidate host by name or ip """
-    # Cortesy of http://stackoverflow.com/questions/106179/regular-expression-to-match-hostname-or-ip-address
-    ValidIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
-    ValidHostnameRegex = "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
-    if re.match(ValidHostnameRegex, str(IP)) or re.match(ValidIpAddressRegex, str(IP)):
-      return True
-    else:
-      return False
-
-  def getInfo(self, host):
-    """ Get info about host """
+def get_info(host):
+    """Get localization information about a host."""
+    url = "http://freegeoip.net/json/%s" % host
+    valid_ip = r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"
+    if not re.match(valid_ip, host):
+        print("/!\ '%s' is not a valid IP, please enter a valid one! /!\\" % host)
+        sys.exit(-1)
     try:
-      if not self.validate_host('google.com'):
-         print "The IP or Hostname is not valid, please enter a valid one ;-)"
-         sys.exit(2)  
-      else:
-         req = urllib2.Request(self.url+host)
-         response = urllib2.urlopen(req)
-         return response
-    except Exception as e:
-      print "An error has occured: {0}".format(e)
-      sys.exit(2)
+       request = urllib2.Request(url)
+       response = urllib2.urlopen(request)
+       return response
+    except:
+       print("/!\ An error has occured, more info bellow /!\\")
+       print(traceback.format_exc())
+       print("/!\ Quitting now! /!\\")
+       sys.exit(-1)
         	 
-         
-
 def usage():
-  print """SimpleGeoIP [hostname | IP] 
-              examples: SimpleGeoIP www.google.com
-                        SimpleGeoIP 173.194.37.64"""
+    print("{0} [ hostname | ip ] \n\t\tExample: {0} 173.194.37.64".format(sys.argv[0]))
 
 def main():
-  """ Metodo main """
-  result = []
-  if len(sys.argv) > 2:
-    handler = GeoIP(sys.argv[1])
-    result = handler.getInfo(sys.argv[2])
-  elif len(sys.argv) == 2:
-    handler = GeoIP()
-    result = handler.getInfo(sys.argv[1])
-  else:
-    usage()
-    sys.exit(2)
+    result = []
+    if len(sys.argv) >= 2:
+       result = get_info(sys.argv[1])
+    else:
+       usage()
+       sys.exit(-1)
+    info = json.load(result)
+    # Delete all keys with empty values
+    for k in info.keys():
+        if not info[k]: del info[k]
 
-  info = json.load(result)
-  # Delete all keys with empty values
-  for x in list(info.keys()):
-    if info[x] == '':
-        del info[x]
-  print "=== SHOWING INFORMATION ABOUT ==="
-  print "Country Code: %s" % info.get('country_code','Not available')
-  print "Country: %s" % info.get('country_name', 'Not available')
-  print "Region Code: %s" % info.get('region_code','Not available')
-  print "City: %s" % info.get('city', 'Not available')
-  print "Zip Code: %s" % info.get('zipcode', 'Not available')
-  print "Latitude: %s" % info.get('latitude', 'Not available')
-  print "Longitude: %s" % info.get('longitude', 'Not available')
-  print "Metro Code: %s" % info.get('metro_code','Not available')
-  print "Area Code: %s" % info.get('areacode', 'Not available')
+    print("########    SHOWING LOCATION INFORMATION ABOUT HOST %s    ########" % sys.argv[1])
+    print("\tCountry Code: %s" % info.get('country_code','Not available'))
+    print("\tCountry: %s" % info.get('country_name', 'Not available'))
+    print("\tRegion Code: %s" % info.get('region_code','Not available'))
+    print("\tCity: %s" % info.get('city', 'Not available'))
+    print("\tZip Code: %s" % info.get('zipcode', 'Not available'))
+    print("\tLatitude: %s" % info.get('latitude', 'Not available'))
+    print("\tLongitude: %s" % info.get('longitude', 'Not available'))
+    print("\tMetro Code: %s" % info.get('metro_code','Not available'))
+    print("\tArea Code: %s" % info.get('areacode', 'Not available'))
+    lat, lon = info.get('latitude'), info.get('longitude')
+    if lat and lon:
+        map_url = "https://www.openstreetmap.org/#map=12/%s/%s" % (lat, lon) 
+        print("See location of %s on map => %s" % (sys.argv[1], map_url))
+
 
 if __name__ == "__main__":
     main()
 
-  
